c4b630e8a1a09f844dceb98a4e6b3e1fc783e8dc
Sebastian Simarro <ssebastian210@gmail.com>
2021-10-25 10:36:49 -0300

    <fix><#main> height vh

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Stack } from '@chakra-ui/layout';
+import { Stack, Center } from '@chakra-ui/layout';
 import BtnAlegria from './BtnAlegria';
 
 const Main = () => {
   return (
-    <Stack p={55} mt={10}>
+    <Stack p={55} mt={10} height="82vh">
       <BtnAlegria />
     </Stack>
   );
